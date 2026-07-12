0050805dd0f5d2bc41bb18dc2e1cff3ce101e8a8
aafanasy <stAnttia@gmail.com>
2026-07-13 01:05:58 +0500

    feat: open public product demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import { OperationsApp } from './ui/operations-app'
-import { redirect } from 'next/navigation'
-import { getPilotSession } from '@/lib/auth'
 
-export default async function Home() {
-  if (!(await getPilotSession())) redirect('/login')
+export default function Home() {
   return <OperationsApp />
 }
