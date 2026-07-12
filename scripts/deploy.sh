39e7c97929f5264435734f2ba45c7d438cad1dc9
aafanasy <stAnttia@gmail.com>
2026-07-12 23:48:49 +0500

    chore: add production deployment override

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,7 @@ set -eu
 
 git fetch origin main
 git reset --hard origin/main
-docker compose up -d --build --remove-orphans
+docker compose -f compose.yaml -f compose.prod.yaml up -d --build --remove-orphans
+for migration in db/migrations/*.sql; do
+  docker compose -f compose.yaml -f compose.prod.yaml exec -T postgres psql -v ON_ERROR_STOP=1 -U zvona -d zvona < "$migration" || true
+done
